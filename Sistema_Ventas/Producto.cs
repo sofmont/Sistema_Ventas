89c74c7d541c10fe4c8192d7af3110609feaf0d6
sofmont <barbaramontalvo01234@gmail.com>
2026-02-24 21:27:19 -0600

    Se creo la clase producto

--- Sistema_Ventas/Producto.cs
+++ Sistema_Ventas/Producto.cs
@@ -8,6 +8,54 @@ namespace Sistema_Ventas
 {
     internal class Producto
     {
+        private string nombre;
+        private decimal precio;
+        private int cantidad;
 
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public decimal Precio
+            { 
+            get { return precio; }
+            set { precio = value; } 
+        }
+
+        public int Cantidad
+        {
+            get { return cantidad; }
+            set { cantidad = value; }
+        }
+         
+        public Producto()
+        {
+            Nombre = "";
+            Precio = 0;
+            Cantidad = 0;
+        }
+
+        public Producto(string nombre, decimal precio, int cantidad)
+        {
+            this.Nombre = nombre;
+            this.Precio = precio;
+            this.Cantidad = cantidad;
+           
+        }
+        
+        public void AgregarProducto(int cantidadadañadida)
+        {
+            if (cantidadadañadida > 0)
+            {
+                Cantidad += cantidadadañadida;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Nombre} - Precio: {Precio:C} (Disponibles: {Cantidad})";
+        }
     }
+      
 }
