1ed6e8b5bb761bcf7827c072d8f3b9bb389fdb07
sofmont <barbaramontalvo01234@gmail.com>
2026-02-24 21:27:28 -0600

    se añadio la clase de ventas

---
+++ Sistema_Ventas/Venta.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Ventas
+{
+    internal class Venta
+    {
+
+        private Producto producto;
+        private int cantidad;
+
+        public Venta(Producto producto, int cantidad)
+        {
+            this.producto = producto;
+            this.cantidad = cantidad;
+        }
+
+        public decimal TotalaPagar()
+        {
+
+            return producto.Precio * cantidad;
+        }
+
+        public bool Procesar()
+        {
+
+            producto.Cantidad += cantidad;
+            return true;
+        }
+
+        
+        public void ActualizarCantidad()
+        {
+            producto.Cantidad -= cantidad;
+        }
+        
+    }
+}
